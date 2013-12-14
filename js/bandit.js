d55848300cc222536dc1bdd8a6a8f4172191efbc
Gabriel Dehan <dehan.gabriel@gmail.com>
2013-12-14 23:10:01 +0100

    Created first object

--- js/bandit.js
+++ js/bandit.js
@@ -1,11 +1,19 @@
 $(function(){
-    piano = Object.create(piano);
-    piano.init(1);
-    console.log(piano.id);
+    var keyboard = Object.create(instrument);
+    keyboard.init();
+    console.log(keyboard.keys);
 });
 
-var piano = {
-    init: function(id) {
-        this.id = id
+var instrument = {
+    init: function() {
+        this.keys = {
+            'la': 'A',
+            'si': 'B',
+            'do': 'C',
+            're': 'D',
+            'mi': 'E',
+            'fa': 'F',
+            'sol': 'G'
+        }
     }
 };
